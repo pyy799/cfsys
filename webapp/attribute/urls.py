62b1536f819d6ac81007ec5d6c41e8e9a460905f
ronger-git <zyr_xx@126.com>
2019-10-10 17:43:05 +0800

    属性第一阶段

--- webapp/attribute/urls.py
+++ webapp/attribute/urls.py
@@ -1,8 +1,9 @@
 from django.conf.urls import include, url
 from django.contrib import admin
-from webapp import views
+from webapp.attribute import views
 
 urlpatterns = [
     # 属性管理
     url(r'^page_attribute/$', views.index, {"template_name": "attribute.html"}, name="page_attribute"),
+    # url(r'^attribute_infor/data/$', views.attribute_infor, name="attribute_infor"),
 ]
--- webapp/attribute/views.py
+++ webapp/attribute/views.py
@@ -10,20 +10,57 @@ from django.shortcuts import render
 from webapp.models import *
 from webapp.shortcuts.ajax import ajax_success, ajax_error
 from cfsys.settings import *
+from webapp.utils.query import get_query, create_data
+from django.views.decorators.csrf import csrf_exempt
 
 
 from django.http import HttpResponse
 from django.shortcuts import render
+from webapp.models import *
+from webapp.upattribute import import_attribute
+
+def index(request,template_name):
+    # attibute_list = Attribute
+    # t = Attribute
+    # import_attribute()
+    attributes = Attribute.objects.all()
+    # result = {}
+    if request.method == "POST":
+        attribute = request.POST.get("attribute_add")
+        first_class = request.POST.get("first_class_add")
+        second_class = request.POST.get("second_class_add", None)
+        meaning = request.POST.get("meaning_add")
+        information = request.POST.get("information_add", None)
+        if second_class:
+            ACT = 't'
+        else:
+            ACT = 'c'
+        attr = Attribute.objects.create(ACT=ACT,attribute=attribute,first_class=first_class,second_class=second_class,meaning=meaning,information=information)
+        attr.save()
+    return render(request,template_name,{'attributes': attributes})
+
+     #    if attribute != None and first_class != None:
+     #        if  second_class == None:
+     #            ACT='c'
+     #        elif second_class !=None:
+     #            ACT='t'
+     #        attr = Attribute.objects.create(ACT=ACT,attribute=attribute,first_class=first_class,second_class=second_class,meaning=meaning,information=information)
+     #        attr.save()
+     #        result['statu'] = 'success'
+     #    else:
+     #        result['statu'] = 'error'
+     #
+     # return render(request,template_name,{'attributes': attributes,"result":result})
 
 
-def index(request, template_name):
-
-    return render(request, template_name)
 
 
 def jump(request, template_name):
-
     return render(request, template_name)
 
-
-
+# @csrf_exempt
+# def attribute_infor(request):
+#     fil = {"ACT": AttributeType.A, "is_vaild": True}
+#     attribute_list, count, error= get_query(request, Attribute, **fil)
+#     res = create_data(request.POST.get("draw", 1), attribute_list, count)
+#     return HttpResponse(res)
