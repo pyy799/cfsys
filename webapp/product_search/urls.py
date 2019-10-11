413632bc845732a7cd87fb4d1278495c5be9ad44
littlelingna <kln54321@163.com>
2019-10-11 11:47:24 +0800

    修改base1.html，index.html以及查询模块部分

--- webapp/product_search/urls.py
+++ webapp/product_search/urls.py
@@ -1,9 +1,11 @@
 from django.conf.urls import include, url
 from django.contrib import admin
 from webapp import views
+from webapp.product_search import views
 
 urlpatterns = [
     # 产品查询
     url(r'^page_search_product/$', views.index, {"template_name": "search_product.html"}, name="page_search_product"),
-    url(r'^page_show_product/$',views.index,{"template_name" : "show_product.html"},name="page_show_product"),
+    url(r'^page_show_product/$', views.index, {"template_name": "show_product.html"}, name="page_show_product"),
+    url(r'^search/data/$',views.search , name="search"),
 ]
