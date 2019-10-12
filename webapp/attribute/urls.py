2c64107f76b9f1ba14def701c8e1a85f54e7b7af
ronger-git <zyr_xx@126.com>
2019-10-12 14:11:26 +0800

    base1和属性

--- webapp/attribute/urls.py
+++ webapp/attribute/urls.py
@@ -5,5 +5,6 @@ from webapp.attribute import views
 urlpatterns = [
     # 属性管理
     url(r'^page_attribute/$', views.index, {"template_name": "attribute.html"}, name="page_attribute"),
+    url(r'^page_attribute/delete/(\d+)/$', views.del_class, name="del_class")
     # url(r'^attribute_infor/data/$', views.attribute_infor, name="attribute_infor"),
 ]
