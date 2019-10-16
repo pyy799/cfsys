6ca7394b35754b8846a2284ca17658e8a5bf22b2
littlelingna <kln54321@163.com>
2019-10-16 15:15:08 +0800

    查询，，

--- webapp/product_search/views.py
+++ webapp/product_search/views.py
@@ -4,7 +4,7 @@ import shutil
 import calendar
 import datetime
 import xlrd
-from django.http import HttpResponse, HttpResponseRedirect
+from django.http import HttpResponse, HttpResponseRedirect, JsonResponse
 from django.shortcuts import render
 from django.views.decorators.csrf import csrf_exempt
 from django.contrib.auth.decorators import login_required
@@ -22,13 +22,18 @@ def index(request, template_name):
     """
     page_dict = {}
     pCompany = COMPANY_CHOICE
-    # maturity =
-    # independence =
-    # business =
-    # technology =
-    page_dict.update({"pCompany": pCompany})
-    return render(request, template_name, page_dict)
+    fil1 = {"ACT": 'c', "attribute": 'M'}
+    maturity = Attribute.objects.filter(**fil1)
+    fil2 = {"ACT": 'c', "attribute": 'I'}
+    independence = Attribute.objects.filter(**fil2)
+    fil3 = {"ACT": 'c', "attribute": 'B'}
+    business = Attribute.objects.filter(**fil3)
+    fil4 = {"ACT": 'c', "attribute": 'T'}
+    technology = Attribute.objects.filter(**fil4)
 
+    page_dict.update({"pCompany": pCompany, "maturity": maturity,
+                      "independence": independence, "business": business, "technology": technology})
+    return render(request, template_name, page_dict)
 
 
 def jump(request, template_name):
@@ -39,8 +44,59 @@ def jump(request, template_name):
 # 查询页签数据
 def search(request):
     # fil = {"status": ProductStatus.PASS, "is_vaild": True}
+
     fil = {"is_vaild": True}
     product_list, count, error = get_query(request, Product, **fil)
     pack_list = [i.pack_data() for i in product_list]
     res = create_data(request.POST.get("draw", 1), pack_list, count)
     return HttpResponse(res)
+
+
+@csrf_exempt
+# 展示页面数据
+def show(request, template_name):
+    """
+    查询/展示 页面
+    """
+    page_dict = {}
+    pCompany = COMPANY_CHOICE
+    fil1 = {"ACT": 'c', "attribute": 'M'}
+    maturity = Attribute.objects.filter(**fil1)
+    fil2 = {"ACT": 'c', "attribute": 'I'}
+    independence = Attribute.objects.filter(**fil2)
+    fil3 = {"ACT": 'c', "attribute": 'B'}
+    business = Attribute.objects.filter(**fil3)
+    fil4 = {"ACT": 'c', "attribute": 'T'}
+    technology = Attribute.objects.filter(**fil4)
+    maturity_product = {}
+    independence_product = {}
+    business_product = {}
+    technology_product = {}
+    for i in maturity:
+        product_list = Product.objects.filter(maturity__first_class=i.first_class)
+        num = len(product_list)
+        # print(num)
+        maturity_product.update({i.meaning: num})
+    for i in independence:
+        product_list = Product.objects.filter(independence__first_class=i.first_class)
+        num = len(product_list)
+        # print(num)
+        independence_product.update({i.meaning: num})
+    for i in business:
+        product_list = Product.objects.filter(business__first_class=i.first_class)
+        num = len(product_list)
+        # print(num)
+        business_product.update({i.meaning: num})
+    for i in technology:
+        product_list = Product.objects.filter(technology__first_class=i.first_class)
+        num = len(product_list)
+        # print(num)
+        technology_product.update({i.meaning: num})
+    print(maturity_product)
+    page_dict.update({"pCompany": pCompany, "maturity": maturity,
+                      "independence": independence, "business": business, "technology": technology,
+                      "maturity_product": maturity_product,
+                      "independence_product": independence_product,
+                      "business_product": business_product,
+                      "technology_product": technology_product})
+    return render(request, template_name, page_dict)
--- webapp/static/js/product/searchproduct.js
+++ webapp/static/js/product/searchproduct.js
@@ -32,8 +32,6 @@ var SearchTable = function () {
                     {"mData": "id", "sTitle": "ID", "bVisible": false},
                     {"mData": "product_name", "sTitle": "产品名称"},
                     {"mData": "pCompany_name", "sTitle": "公司名称"},
-                    // {"mData": "one_year_money", "sTitle": "过去一年销售额"},
-                    // {"mData": "three_year_money", "sTitle": "过去三年销售额"},
                     {"mData": "maturity_name", "sTitle": "成熟度"},
                     {"mData": "independence_name", "sTitle": "自主度"},
                     {"mData": "business_name", "sTitle": "业务领域"},
