5b6828299d1ecd3a3e70709fa1909e1cf341ad3f
chenjiwei <17801006742@163.com>
2019-10-24 13:36:29 +0800

    这是分页和判断名称是否为空及重复之后的结果

--- webapp/user/urls.py
+++ webapp/user/urls.py
@@ -3,12 +3,16 @@ from django.contrib import admin
 from webapp.user import views
 
 urlpatterns = [
-    # 用户权限管理
-    url(r'^page_user_management/$', views.jump, {"template_name": "user_management.html"}, name="page_user_management"),
+
+    url(r'^page_user_management/$', views.jump_user, {"template_name": "user_management.html"}, name="page_user_management"),
     url(r'^page_user_management/add_user/$', views.add_user, name="add_user"),
-    url(r'^page_user_management/delete/(\d+)/$', views.delete_user, name= "delete_user"),
-    url(r'^page_role_management/$', views.index, {"template_name": "role_management.html"}, name="page_role_management"),
+    url(r'^page_user_management/delete/(\d+)/$', views.delete_user, name="delete_user"),
     url(r'^page_user_management/active$', views.active_user, name="active_user"),
     url(r'^page_user_management/modify/$', views.modify_user, name="modify_user"),
-    url(r'^page_user_management/search/$', views.search_user, name="search_user")
+    url(r'^page_user_management/search/$', views.search_user, name="search_user"),
+    url(r'^page_role_management/$', views.jump_role, {"template_name": "role_management.html"}, name="page_role_management"),
+    url(r'^page_role_management/add_role/$', views.add_role, name="add_role"),
+    url(r'^page_role_management/modify/$', views.modify_role, name="modify_role"),
+    url(r'^page_role_management/delete$',views.delete_role,name="delete_role"),
+    url(r'^page_user_management/user_check$',views.user_check,name="user_add_check")
 ]
--- webapp/user/views.py
+++ webapp/user/views.py
@@ -3,24 +3,27 @@ from django.shortcuts import render
 from django.contrib.auth.decorators import login_required, permission_required
 from django.views.decorators.csrf import csrf_exempt
 from webapp.models import UserProfile, Group
-
-
-def index(request, template_name):
-
-    return render(request, template_name)
+from django.contrib.auth.models import Permission
 
 
 @login_required
 @permission_required('webapp.user_right_management_user')
-def jump(request, template_name):
-    users = UserProfile.objects.all()
+def jump_user(request, template_name):
+    page = 1
+    pageLimit = 2
+    userCounts = UserProfile.objects.all().count()
+    if userCounts % pageLimit != 0:
+        pageCounts = int(userCounts/pageLimit)+1
+    else:
+        pageCounts = int(userCounts/pageLimit)
+    users = UserProfile.objects.all().order_by("-id")[(page-1)*pageLimit:page*pageLimit]
     groups = Group.objects.all().order_by("id")
     for user in users:
         user.date_joined = user.date_joined.strftime('%Y-%m-%d')
         user.Company = user.get_company(user.uCompany)
         gr = Group.objects.get(user=user)
         user.group = gr.name
-    return render(request, template_name, {"users": users, "groups":groups})
+    return render(request, template_name, {"users": users, "groups": groups, "pageCounts": pageCounts})
 
 
 @login_required
@@ -63,9 +66,13 @@ def active_user(request):
 @login_required
 @permission_required('webapp.user_right_management_user')
 def modify_user(request):
+    # 判断数据库中是否已经存在该用户名称
+    userCount = UserProfile.objects.filter(username=request.POST.get("username").strip()).count()
+    if userCount > 1:
+        return JsonResponse({"status": -1})
     modify_id = request.POST.get("id")
     user = UserProfile.objects.get(id=modify_id)
-    user.username = request.POST.get("username")
+    user.username = request.POST.get("username").strip()
     user.gender = request.POST.get("gender")
     user.uCompany = request.POST.get("uCompany")
     user.department = request.POST.get("department")
@@ -73,6 +80,7 @@ def modify_user(request):
     user.phone = request.POST.get("phone")
     user.email = request.POST.get("email")
     user.save()
+
     gr = Group.objects.get(user=user)
     user.groups.remove(gr)
     group = Group.objects.get(id=request.POST.get("role"))
@@ -86,7 +94,8 @@ def modify_user(request):
         "position": user.position,
         "group": group.name,
         "phone": user.phone,
-        "email": user.email
+        "email": user.email,
+        "status": 0
     }
     return JsonResponse(res)
 
@@ -95,6 +104,9 @@ def modify_user(request):
 @login_required
 @permission_required('webapp.user_right_management_user')
 def search_user(request):
+    # 获得页数
+    page = int(request.POST.get("page"))
+    pageLimit = 2
     # 获得查询条件
     username = request.POST.get("username")
     uCompany = request.POST.get("uCompany")
@@ -114,12 +126,19 @@ def search_user(request):
         search_dict["groups__id"] = int(group)
     print(search_dict)
     if not search_dict:
-        user_set = UserProfile.objects.all()
+        userCounts = UserProfile.objects.all().count()
     else:
-        user_set = UserProfile.objects.filter(**search_dict)
+        userCounts = UserProfile.objects.filter(**search_dict).count()
+    if userCounts % pageLimit != 0:
+        pageCounts = int(userCounts/pageLimit)+1
+    else:
+        pageCounts = int(userCounts/pageLimit)
+    if not search_dict:
+        user_set = UserProfile.objects.all().order_by("-id")[(page-1)*pageLimit:page*pageLimit]
+    else:
+        user_set = UserProfile.objects.filter(**search_dict).order_by("-id")[(page-1)*pageLimit:page*pageLimit]
     data_list = []
     for user in user_set:
-        print(user)
         gr = Group.objects.get(user=user)
         user_dict = {
             "id": user.id,
@@ -135,5 +154,134 @@ def search_user(request):
             "is_active": user.is_active,
         }
         data_list.append(user_dict)
+    return JsonResponse({"status": 0, "users": data_list,"pageCounts": pageCounts,"userCounts": userCounts})
 
-    return JsonResponse({"status": 0, "users": data_list})
+
+@login_required
+@permission_required('webapp.user_right_management_role')
+def jump_role(request, template_name):
+    group_set = Group.objects.all().order_by("id")
+    group_list = []
+    for group in group_set:
+        perm_list = []
+        permissions = group.permissions.all()
+        for permission in permissions:
+            perm_list.append(permission.codename)
+        group_dic = {"id": group.id, "name": group.name, "perm_list":perm_list}
+        group_list.append(group_dic)
+    # return JsonResponse({"group_list":group_list})
+    return render(request, template_name, {"group_list": group_list})
+
+
+@login_required
+@permission_required('webapp.user_right_management_role')
+def add_role(request):
+    group_name = request.POST.get("group_name").strip()
+    groupCount = Group.objects.filter(name=group_name).count()
+    if groupCount != 0:
+        return JsonResponse({"status": -1})
+    group = Group()
+    group.name = group_name
+    group.save()
+    perm_list = []
+    if request.POST.get("perm1") == "true":
+        permission = Permission.objects.get(codename="product_information_inquiry")
+        perm_list.append("product_information_inquiry")
+        group.permissions.add(permission)
+    if request.POST.get("perm2") == "true":
+        permission = Permission.objects.get(codename="product_information_manage_new")
+        perm_list.append("product_information_manage_new")
+        group.permissions.add(permission)
+    if request.POST.get("perm3") == "true":
+        permission = Permission.objects.get(codename="product_information_manage_update")
+        perm_list.append("product_information_manage_update")
+        group.permissions.add(permission)
+    if request.POST.get("perm4") == "true":
+        permission = Permission.objects.get(codename="product_information_manege_check")
+        perm_list.append("product_information_manege_check")
+        group.permissions.add(permission)
+    if request.POST.get("perm5") == "true":
+        permission = Permission.objects.get(codename="product_attribute_management")
+        perm_list.append("product_attribute_management")
+        group.permissions.add(permission)
+    if request.POST.get("perm6") == "true":
+        permission = Permission.objects.get(codename="user_right_management_user")
+        perm_list.append("user_right_management_user")
+        group.permissions.add(permission)
+    if request.POST.get("perm7") == "true":
+        permission = Permission.objects.get(codename="user_right_management_role")
+        perm_list.append("user_right_management_role")
+        group.permissions.add(permission)
+    group = Group.objects.get(name=group_name)
+    group_dic = {"id": group.id, "name": group.name, "perm_list": perm_list, "status":0}
+    return JsonResponse(group_dic)
+
+
+@login_required
+@permission_required('webapp.user_right_management_role')
+def modify_role(request):
+    group_name = request.POST.get("group_name").strip()
+    groupCount = Group.objects.filter(name=group_name).count()
+    if groupCount > 1:
+        return JsonResponse({"status": -1})
+    group_id = request.POST.get("group_id")
+    group = Group.objects.get(id=group_id)
+    group.name = group_name
+    group.permissions.clear()
+    group.save()
+    perm_list = []
+    if request.POST.get("perm1") == "true":
+        permission = Permission.objects.get(codename="product_information_inquiry")
+        perm_list.append("product_information_inquiry")
+        group.permissions.add(permission)
+    if request.POST.get("perm2") == "true":
+        permission = Permission.objects.get(codename="product_information_manage_new")
+        perm_list.append("product_information_manage_new")
+        group.permissions.add(permission)
+    if request.POST.get("perm3") == "true":
+        permission = Permission.objects.get(codename="product_information_manage_update")
+        perm_list.append("product_information_manage_update")
+        group.permissions.add(permission)
+    if request.POST.get("perm4") == "true":
+        permission = Permission.objects.get(codename="product_information_manege_check")
+        perm_list.append("product_information_manege_check")
+        group.permissions.add(permission)
+    if request.POST.get("perm5") == "true":
+        permission = Permission.objects.get(codename="product_attribute_management")
+        perm_list.append("product_attribute_management")
+        group.permissions.add(permission)
+    if request.POST.get("perm6") == "true":
+        permission = Permission.objects.get(codename="user_right_management_user")
+        perm_list.append("user_right_management_user")
+        group.permissions.add(permission)
+    if request.POST.get("perm7") == "true":
+        permission = Permission.objects.get(codename="user_right_management_role")
+        perm_list.append("user_right_management_role")
+        group.permissions.add(permission)
+    group_dic = {"id": group_id, "name": group.name, "perm_list": perm_list,"status": 0}
+    return JsonResponse(group_dic)
+
+
+def delete_role(request):
+    delete_id = request.GET.get("id")
+    print(delete_id)
+    group = Group.objects.get(id=delete_id)
+    group.permissions.clear()
+    user_set = UserProfile.objects.filter(groups__id=delete_id)
+    group_temp = Group.objects.get(name="无权限角色")
+    print(user_set.count())
+    if user_set.count() != 0:
+        for user in user_set:
+            user.groups.remove(group)
+            user.groups.add(group_temp)
+    group.delete()
+    return JsonResponse({"status": 0})
+
+
+def user_check(request):
+    username = request.GET.get("username")
+    userCount = UserProfile.objects.filter(username=username.strip()).count()
+    if userCount !=0:
+        return JsonResponse({"status": -1})
+    else:
+        return JsonResponse({"status": 0})
